68283a14f2eedf9e67e0f3b4f38850c8da2d23a5
Mike del Castillo <johnmichaeldc@gmail.com>
2019-06-02 09:39:58 +0800

    hooks

---
+++ test.js
@@ -0,0 +1,18 @@
+// const readline = require('readline');
+// readline.emitKeypressEvents(process.stdin);
+// process.stdin.setRawMode(true);
+// process.stdin.on('keypress', console.log);
+// process.stdin.on('keyrelease', console.log);
+// console.log('Press any key...';)
+
+const ioHook = require('iohook');
+
+// ioHook.on('mousemove', console.log);
+// ioHook.on('keydown', console.log);
+// ioHook.on('keyup', console.log);
+
+// Register and start hook
+// ioHook.start();
+
+// Alternatively, pass true to start in DEBUG mode.
+ioHook.start(true);
