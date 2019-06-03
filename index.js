14710b4d5b81ee0d038ccbe7c6c41852b88a64f8
Mike del Castillo <johnmichaeldc@gmail.com>
2019-06-03 08:33:29 +0800

    starting index

---
+++ index.js
@@ -0,0 +1,37 @@
+const noble = require('noble')
+const ioHook = require('iohook')
+
+let mode = 0
+let discovered = []
+let selected = null
+let keys = {}
+
+function update() {
+    if (mode == 0) { // Select device
+
+    } else if (mode == 1) { // Controll device
+
+    }
+}
+
+noble.on('stateChange', (state) => {
+    console.log('NOBLE[STATE_CHANGE]: ' + state)
+    return state === 'poweredOn' ? noble.startScanning() : noble.stopScanning()
+})
+
+noble.on('scanStart', () => console.log('NOBLE[SCAN]: start'))
+noble.on('scanStop', () => console.log('NOBLE[SCAN]: stop'))
+
+noble.on('discover', peripheral => {
+
+})
+
+ioHook.on('keydown', e => {
+    keys[e.keycode] = true
+    console.log(keys)
+})
+
+ioHook.on('keyup', e => {
+    keys[e.keycode] = false
+    console.log(keys)
+})
--- test.js
+++ test.js
@@ -1,160 +1,156 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
 const noble = require('noble');
 const ioHook = require('iohook');
 const boardUUID = '9059af16ac30';
 
-noble.on('stateChange', function(state) {
-  console.log('on -> stateChange: ', state);
+noble.on('stateChange', function (state) {
+	console.log('on -> stateChange: ', state);
 
-  if (state === 'poweredOn') {
-    noble.startScanning();
-  } else {
-    noble.stopScanning();
-  }
+	if (state === 'poweredOn') {
+		noble.startScanning();
+	} else {
+		noble.stopScanning();
+	}
 });
 
-noble.on('scanStart', function() {
-  console.log('on -> scanStart');
+noble.on('scanStart', function () {
+	console.log('on -> scanStart');
 });
 
-noble.on('scanStop', function() {
-  console.log('on -> scanStop');
+noble.on('scanStop', function () {
+	console.log('on -> scanStop');
 });
 
-noble.on('discover', function(peripheral) {
-  console.log('on -> discover: ', peripheral);
-  if(peripheral.uuid != boardUUID) return false;
+noble.on('discover', function (peripheral) {
+	console.log('on -> discover: ', peripheral);
+	if (peripheral.uuid != boardUUID) return false;
 
-  peripheral.on('connect', function() {
-    console.log('on -> connect');
-    this.updateRssi();
-  });
+	peripheral.on('connect', function () {
+		console.log('on -> connect');
+		this.updateRssi();
+	});
 
-  peripheral.on('disconnect', function() {
-    console.log('on -> disconnect', peripheral);
-  });
+	peripheral.on('disconnect', function () {
+		console.log('on -> disconnect', peripheral);
+	});
 
-  peripheral.on('rssiUpdate', function(rssi) {
-    console.log('on -> RSSI update ', rssi);
-    this.discoverServices();
-  });
+	peripheral.on('rssiUpdate', function (rssi) {
+		console.log('on -> RSSI update ', rssi);
+		this.discoverServices();
+	});
 
-  peripheral.on('servicesDiscover', function(services) {
-    console.log('on -> peripheral services discovered ', services);
+	peripheral.on('servicesDiscover', function (services) {
+		console.log('on -> peripheral services discovered ', services);
 
-    services.forEach((service, serviceIndex) => {
+		services.forEach((service, serviceIndex) => {
 
-      services[serviceIndex].on('includedServicesDiscover', function(includedServiceUuids) {
-        console.log('on -> service included services discovered ', includedServiceUuids);
-        this.discoverCharacteristics();
-      });
+			services[serviceIndex].on('includedServicesDiscover', function (includedServiceUuids) {
+				console.log('on -> service included services discovered ', includedServiceUuids);
+				this.discoverCharacteristics();
+			});
 
-      services[serviceIndex].on('characteristicsDiscover', function(characteristics) {
-        console.log('on -> service characteristics discovered ', characteristics);
-        characteristics.forEach((char, characteristicIndex) => {
-          if(char.uuid != 'dfb1') return false;
-          //
-          // characteristics[characteristicIndex].on('read', function(data, isNotification) {
-          //   console.log('on CHAR READ!!! ' + data + ' ', isNotification);
-          //   console.log(data.toString());
-          //
-          //   // peripheral.disconnect();
-          // });
-          //
-          // characteristics[characteristicIndex].on('write', function() {
-          //   console.log('on -> characteristic write ');
-          //
-          //   // peripheral.disconnect();
-          // });
+			services[serviceIndex].on('characteristicsDiscover', function (characteristics) {
+				console.log('on -> service characteristics discovered ', characteristics);
+				characteristics.forEach((char, characteristicIndex) => {
+					if (char.uuid != 'dfb1') return false;
+					//
+					// characteristics[characteristicIndex].on('read', function(data, isNotification) {
+					//   console.log('on CHAR READ!!! ' + data + ' ', isNotification);
+					//   console.log(data.toString());
+					//
+					//   // peripheral.disconnect();
+					// });
+					//
+					// characteristics[characteristicIndex].on('write', function() {
+					//   console.log('on -> characteristic write ');
+					//
+					//   // peripheral.disconnect();
+					// });
 
-          characteristics[characteristicIndex].on('broadcast', function(state) {
-            console.log('on -> characteristic broadcast ', state);
+					characteristics[characteristicIndex].on('broadcast', function (state) {
+						console.log('on -> characteristic broadcast ', state);
 
-            // peripheral.disconnect();
-          });
+						// peripheral.disconnect();
+					});
 
-          characteristics[characteristicIndex].on('notify', function(state) {
-            console.log('on -> characteristic notify ', state);
+					characteristics[characteristicIndex].on('notify', function (state) {
+						console.log('on -> characteristic notify ', state);
 
-            // peripheral.disconnect();
-          });
+						// peripheral.disconnect();
+					});
 
-          // setInterval(() => {
-          //   char.read(function (error, data) {
-          //     console.log(error, data.toString())
-          //   });
-          // }, 100);
+					// setInterval(() => {
+					//   char.read(function (error, data) {
+					//     console.log(error, data.toString())
+					//   });
+					// }, 100);
 
-        // char.read();
+					// char.read();
 
-        function send(str){
-          const message = new Buffer(str.toString());
-          char.write(
-            message,
-            true,
-            function (error) {
-              if (!error) {
-                console.log(str, 'write succesfull');
-              } else {
-                console.log('write unsuccessfull');
-              }
-          }.bind(this));
-        }
+					function send(str) {
+						const message = new Buffer(str.toString());
+						char.write(
+							message,
+							true,
+							function (error) {
+								if (!error) {
+									console.log(str, 'write succesfull');
+								} else {
+									console.log('write unsuccessfull');
+								}
+							}.bind(this));
+					}
 
-        ioHook.on('keydown', e => {
-          console.log(e.keycode);
-          // let d = {
-          //   87: 2,
-          //   83: 4,
-          //   65: 6,
-          //   68: 8,
-          // };
+					ioHook.on('keydown', e => {
+						console.log(e.keycode);
+						// let d = {
+						//   87: 2,
+						//   83: 4,
+						//   65: 6,
+						//   68: 8,
+						// };
 
-          81 - 65
-          87 - 83
+						81 - 65
+						87 - 83
 
-          let d = {
-            87: 2,
-            83: 4,
-            81: 6,
-            65: 8,
-          };
+						let d = {
+							87: 2,
+							83: 4,
+							81: 6,
+							65: 8,
+						};
 
-          let s = d[e.keycode];
-          if(s) send(s);
-        }, false);
+						let s = d[e.keycode];
+						if (s) send(s);
+					}, false);
 
-        ioHook.on('keyup', e => {
-          // let d = {
-          //   87: 1,
-          //   83: 3,
-          //   65: 5,
-          //   68: 7,
-          // };
+					ioHook.on('keyup', e => {
+						// let d = {
+						//   87: 1,
+						//   83: 3,
+						//   65: 5,
+						//   68: 7,
+						// };
 
-          let d = {
-            87: 1,
-            83: 3,
-            81: 5,
-            65: 7,
-          };
+						let d = {
+							87: 1,
+							83: 3,
+							81: 5,
+							65: 7,
+						};
 
-          let s = d[e.keycode];
-          if(s) send(s.toString());
-        }, false);
+						let s = d[e.keycode];
+						if (s) send(s.toString());
+					}, false);
 
-        });
-      });
-
-
-      services[serviceIndex].discoverIncludedServices();
-    });
+				});
+			});
 
 
-  });
+			services[serviceIndex].discoverIncludedServices();
+		});
 
-  peripheral.connect();
+
+	});
+
+	peripheral.connect();
 });
